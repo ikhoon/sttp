e4a3c8c8f9fa817c6920ea7baedda6a091ea467c
Ikhun Um <ih.pert@gmail.com>
2020-01-27 04:58:30 +0900

    Fix compile error

--- armeria-backend/src/main/scala/sttp/client/armeria/ArmeriaBackend.scala
+++ armeria-backend/src/main/scala/sttp/client/armeria/ArmeriaBackend.scala
@@ -1,41 +1,60 @@
 package sttp.client.armeria
 
-import java.io.InputStream
-import java.net.http.HttpRequest.BodyPublisher
+import java.io.{ByteArrayInputStream, File, InputStream}
+import java.nio.ByteBuffer
 import java.nio.file.{Files, Path}
-import java.util.Map
-import java.util.concurrent.ThreadLocalRandom
+import java.util.concurrent.{ConcurrentLinkedQueue, ThreadLocalRandom}
 
+import com.github.ghik.silencer.silent
 import com.linecorp.armeria.client.WebClient
 import com.linecorp.armeria.common._
+import com.linecorp.armeria.common.stream.{AbortedStreamException, CancelledSubscriptionException}
 import io.netty.buffer.Unpooled
-import io.netty.util.AsciiString
 import org.reactivestreams.{Publisher, Subscriber, Subscription}
-import sttp.client.internal.{CrLf, Iso88591}
+import sttp.client
+import sttp.client.internal.{CrLf, FileHelpers, Iso88591}
 import sttp.client.monad.MonadAsyncError
-import sttp.client.{BasicRequestBody, ByteArrayBody, ByteBufferBody, FileBody, IgnoreResponse, InputStreamBody, MappedResponseAs, MultipartBody, NoBody, NothingT, Request, Response, ResponseAs, ResponseAsByteArray, ResponseAsFile, ResponseAsFromMetadata, ResponseAsStream, ResponseMetadata, StreamBody, StringBody, SttpBackend}
-import sttp.model.{Header, HeaderNames, Method, StatusCode}
 import sttp.client.monad.syntax._
+import sttp.client.{
+  BasicRequestBody,
+  ByteArrayBody,
+  ByteBufferBody,
+  FileBody,
+  IgnoreResponse,
+  InputStreamBody,
+  MappedResponseAs,
+  MultipartBody,
+  NoBody,
+  NothingT,
+  Request,
+  Response,
+  ResponseAs,
+  ResponseAsByteArray,
+  ResponseAsFile,
+  ResponseAsFromMetadata,
+  ResponseAsStream,
+  ResponseMetadata,
+  StreamBody,
+  StringBody,
+  SttpBackend
+}
+import sttp.model.{Header, HeaderNames, Method, StatusCode}
 
-import scala.compat.java8.FutureConverters._
 import scala.collection.JavaConverters._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 import scala.util.Try
 import scala.util.control.NonFatal
 
+// TODO(ikhoon): Add HttpRequestEncoder, HttpResponseDecoder and test case
 abstract class ArmeriaBackend[F[_], S](
-                                        webClient: WebClient,
-                                        private implicit val monad: MonadAsyncError[F],
-                                        closeClient: Boolean
-                                      ) extends SttpBackend[F, S, NothingT] {
+    webClient: WebClient,
+    private implicit val monad: MonadAsyncError[F],
+    closeClient: Boolean
+) extends SttpBackend[F, S, NothingT] {
   override def send[T](request: Request[T, S]): F[Response[T]] = {
     val value: F[HttpRequest] = monad.fromTry(Try(toArmeriaRequest(request)))
     value.flatMap(req => {
-      webClient.execute(req)
-      request.response
-      // TODO
-      ???
+      val response = webClient.execute(req)
+      fromArmeriaResponse(request.response, response)
     })
   }
 
@@ -45,13 +64,125 @@ abstract class ArmeriaBackend[F[_], S](
 
   protected def fileToPublisher(path: Path): Option[Publisher[HttpData]] = None
 
+  def responseToBodyPublisher(p: Publisher[HttpObject]): F[(ResponseHeaders, Publisher[HttpData])] = {
+    monad.async[(ResponseHeaders, Publisher[HttpData])](cb => {
+      val publisher: Publisher[HttpData] = new Publisher[HttpData] { pub =>
+        override def subscribe(s: Subscriber[_ >: HttpData]): Unit = {
+          p.subscribe(new Subscriber[HttpObject] {
+            var subscription: Subscription = _
+            var headerReceived: Boolean = false
+
+            override def onError(t: Throwable): Unit = {
+              if (!headerReceived) {
+                t match {
+                  case _: CancelledSubscriptionException | _: AbortedStreamException =>
+                    cb(Right((ResponseHeaders.of(HttpStatus.UNKNOWN), pub)))
+                  case _ =>
+                    cb(Left((t)))
+                }
+              }
+              s.onError(t)
+            }
+
+            override def onComplete(): Unit = {
+              if (!headerReceived) {
+                cb(Right((ResponseHeaders.of(HttpStatus.UNKNOWN), pub)))
+              }
+              s.onComplete()
+            }
+
+            override def onNext(t: HttpObject): Unit = {
+              t match {
+                case data: HttpData =>
+                  s.onNext(data)
+                case headers: ResponseHeaders =>
+                  headerReceived = true
+                  if (headers.status().codeClass() != HttpStatusClass.INFORMATIONAL) {
+                    cb(Right((headers, pub)))
+                  }
+                  subscription.request(1)
+              }
+            }
+
+            override def onSubscribe(v: Subscription): Unit = {
+              subscription = v
+              s.onSubscribe(v)
+            }
+          })
+        }
+      }
+    })
+  }
+
+  protected def fromArmeriaResponse[T](responseAs: ResponseAs[T, S], response: HttpResponse): F[Response[T]] = {
+    responseToBodyPublisher(response)
+      .flatMap {
+        case (headers, publisher) =>
+          val httpHeaders = headers
+            .names()
+            .asScala
+            .flatMap(name => headers.getAll(name).asScala.map(Header.notValidated(name.toString, _)))
+            .toList
+          val meta =
+            ResponseMetadata(httpHeaders,
+                             StatusCode.notValidated(headers.status().code()),
+                             headers.status().codeAsText())
+          handleBody(publisher, responseAs, meta)
+            .map(body => {
+              client.Response(
+                body,
+                StatusCode.unsafeApply(headers.status().code()),
+                headers.status.codeAsText(),
+                httpHeaders,
+                Nil
+              )
+            })
+      }
+  }
+
+  private def handleBody[T](publisher: Publisher[HttpData],
+                            responseAs: ResponseAs[T, S],
+                            meta: ResponseMetadata): F[T] = {
+    responseAs match {
+      case MappedResponseAs(raw, g) =>
+        val nested = handleBody(publisher, raw, meta)
+        nested.map(g(_, meta))
+      case ResponseAsFromMetadata(f) => handleBody(publisher, f(meta), meta)
+      case _: ResponseAsStream[_, _] => monad.unit(publisherToStreamBody(publisher).asInstanceOf[T])
+      case IgnoreResponse            =>
+        // getting the body and discarding it
+        publisherToBytes(publisher).map(_ => ())
+      case ResponseAsByteArray =>
+        publisherToBytes(publisher)
+      case ResponseAsFile(file) =>
+        publisherToFile(publisher, file.toFile).map(_ => file)
+    }
+  }
+
+  protected def publisherToStreamBody(p: Publisher[HttpData]): S
+
+  protected def publisherToBytes(p: Publisher[HttpData]): F[Array[Byte]] = {
+    monad.async { cb =>
+      def success(r: ByteBuffer): Unit = cb(Right(r.array()))
+
+      def error(t: Throwable): Unit = cb(Left(t))
+
+      p.subscribe(new SimpleSubscriber(success, error))
+    }
+  }
+
+  protected def publisherToFile(p: Publisher[HttpData], f: File): F[Unit] = {
+    publisherToBytes(p).map(bytes => FileHelpers.saveFile(f, new ByteArrayInputStream(bytes)))
+  }
+
   private def toArmeriaRequest[T](request: Request[T, S]): HttpRequest = {
-    val headers: RequestHeaders = headersToArmeria(request.headers, methodToArmeria(request.method), request.uri.toString)
+    val headers: RequestHeaders =
+      headersToArmeria(request.headers, methodToArmeria(request.method), request.uri.toString)
     request.body match {
       case NoBody => HttpRequest.of(headers)
       case body: BasicRequestBody =>
         toArmeriaBody(body) match {
-          case Left(httpData) => HttpRequest.of(headers, httpData)
+          case Left(httpData)   => HttpRequest.of(headers, httpData)
           case Right(publisher) => HttpRequest.of(headers, publisher)
         }
       case StreamBody(s) =>
@@ -78,10 +209,10 @@ abstract class ArmeriaBackend[F[_], S](
               val bodyLen: Option[Long] = p.body match {
                 case StringBody(b, encoding, _) =>
                   Some(b.getBytes(encoding).length.toLong)
-                case ByteArrayBody(b, _) => Some(b.length.toLong)
-                case ByteBufferBody(_, _) => None
+                case ByteArrayBody(b, _)   => Some(b.length.toLong)
+                case ByteBufferBody(_, _)  => None
                 case InputStreamBody(_, _) => None
-                case FileBody(b, _) => Some(b.toFile.length())
+                case FileBody(b, _)        => Some(b.toFile.length())
               }
 
               val headersLen = headers.getBytes(Iso88591).length
@@ -90,7 +221,7 @@ abstract class ArmeriaBackend[F[_], S](
           }
           .foldLeft(Option(finalBoundaryLen)) {
             case (Some(acc), Some(l)) => Some(acc + l)
-            case _ => None
+            case _                    => None
           }
         val builder = headers.toBuilder
         builder.add(HttpHeaderNames.CONTENT_TYPE, "multipart/form-data; boundary=" + boundary)
@@ -117,68 +248,6 @@ abstract class ArmeriaBackend[F[_], S](
     }
   }
 
-  protected def fromArmeriaResponse[T](responseAs: ResponseAs[T, S], response: HttpResponse): F[Response[T]] = {
-    response.aggregate().thenApply { case res: AggregatedHttpResponse =>
-      val headers = res.headers().names()
-        .asScala
-        .flatMap(name => res.headers().getAll(name).asScala.map(Header.notValidated(name.toString, _)))
-        .toList
-      val responseMetadata = ResponseMetadata(headers, StatusCode.notValidated(res.status().code()), res.status().codeAsText())
-    }
-  }
-
-  abstract class BodyPublisher extends Publisher[HttpData] {
-    var httpHeaders: HttpHeaders = _
-  }
-
-  def toBodyPublisher(response: HttpResponse): F[BodyPublisher] = {
-    monad.async { cb =>
-      new BodyPublisher { self =>
-        override def subscribe(s: Subscriber[_ >: HttpData]): Unit = {
-          response.subscribe(new Subscriber[HttpObject] {
-            @volatile var subscription: Subscription = _
-            override def onSubscribe(v: Subscription): Unit =
-              s.onSubscribe(v)
-
-            override def onNext(t: HttpObject): Unit = t match {
-              case header: HttpHeaders =>
-                self.httpHeaders = header
-                subscription.request(1)
-              case data: HttpData =>
-                s.onNext(data)
-                cb(Right(self))
-            }
-
-            override def onError(t: Throwable): Unit = ???
-
-            override def onComplete(): Unit =
-              s.onComplete()
-          })
-        }
-      }
-    }
-  }
-
-
-  private def fromArmeriaResponse0[T](responseAs: ResponseAs[T, S], response: AggregatedHttpResponse, meta: ResponseMetadata): F[T] = {
-    responseAs match {
-      case MappedResponseAs(raw, g) =>
-        fromArmeriaResponse0(raw, response, meta).map(g(_, meta))
-      case ResponseAsFromMetadata(f) =>
-        fromArmeriaResponse0(f(meta), response, meta)
-      case IgnoreResponse =>
-        monad.unit(())
-      case ResponseAsByteArray =>
-        monad.unit(response.content().array())
-      case ras @ ResponseAsStream() =>
-
-
-      case ResponseAsFile(file) =>
-
-    }
-  }
-
-
   private def toArmeriaBody(body: BasicRequestBody): Either[HttpData, Publisher[HttpData]] =
     body match {
       case StringBody(s, e, _) =>
@@ -201,21 +270,23 @@ abstract class ArmeriaBackend[F[_], S](
 
   private def methodToArmeria(method: Method): HttpMethod =
     method match {
-      case Method.GET => HttpMethod.GET
-      case Method.HEAD => HttpMethod.HEAD
-      case Method.POST => HttpMethod.POST
-      case Method.PUT => HttpMethod.PUT
-      case Method.DELETE => HttpMethod.DELETE
+      case Method.GET     => HttpMethod.GET
+      case Method.HEAD    => HttpMethod.HEAD
+      case Method.POST    => HttpMethod.POST
+      case Method.PUT     => HttpMethod.PUT
+      case Method.DELETE  => HttpMethod.DELETE
       case Method.OPTIONS => HttpMethod.OPTIONS
-      case Method.PATCH => HttpMethod.PATCH
+      case Method.PATCH   => HttpMethod.PATCH
       case Method.CONNECT => HttpMethod.CONNECT
-      case Method.TRACE => HttpMethod.TRACE
+      case Method.TRACE   => HttpMethod.TRACE
     }
 
   private def headersToArmeria(headers: Seq[Header], httpMethod: HttpMethod, path: String) =
-    headers.foldLeft(RequestHeaders.builder(httpMethod, path)) {
-      case (builder, header) => builder.add(header.name, header.value)
-    }.build()
+    headers
+      .foldLeft(RequestHeaders.builder(httpMethod, path)) {
+        case (builder, header) => builder.add(header.name, header.value)
+      }
+      .build()
 
   val rand = ThreadLocalRandom.current()
 
@@ -225,21 +296,25 @@ abstract class ArmeriaBackend[F[_], S](
   private def newBoundary(): String = rand.nextLong().toHexString
 }
 
-
 /**
   * A [[Subscriber]] implementation which writes a streaming response with the contents converted from
   * the objects published from a publisher.
   */
-final private class StreamingSubscriber(
-                                         writer: HttpRequestWriter,
-                                       ) extends Subscriber[HttpData] {
+final private class StreamingSubscriber(writer: HttpRequestWriter) extends Subscriber[HttpData] {
   private var subscription: Subscription = _
   private var headersSent = false
 
   override def onSubscribe(s: Subscription): Unit = {
     assert(subscription == null)
     subscription = s
-    writer.completionFuture().exceptionally { case _: Throwable => s.cancel() }
+    writer.completionFuture().exceptionally {
+      new java.util.function.Function[Throwable, Void] {
+        override def apply(t: Throwable): Void = {
+          s.cancel()
+          null
+        }
+      }
+    }
     s.request(Long.MaxValue)
   }
 
@@ -268,4 +343,41 @@ final private class StreamingSubscriber(
     if (writer.isOpen) {
       writer.close()
     }
+}
+
+private class SimpleSubscriber(success: ByteBuffer => Unit, error: Throwable => Unit) extends Subscriber[HttpData] {
+  private val chunks = new ConcurrentLinkedQueue[Array[Byte]]()
+  private var subscription: Subscription = _
+  private var size = 0
+
+  override def onSubscribe(s: Subscription): Unit = {
+    assert(s != null)
+    if (this.subscription != null) {
+      s.cancel() // Cancel the additional subscription
+    } else {
+      subscription = s
+      subscription.request(Long.MaxValue)
+    }
+  }
+
+  @silent("discarded")
+  override def onNext(b: HttpData): Unit = {
+    assert(b != null)
+    val a = b.array()
+    size += a.length
+    chunks.add(a)
+  }
+
+  override def onError(t: Throwable): Unit = {
+    assert(t != null)
+    chunks.clear()
+    error(t)
+  }
+
+  override def onComplete(): Unit = {
+    val result = ByteBuffer.allocate(size)
+    chunks.asScala.foreach(result.put)
+    chunks.clear()
+    success(result)
+  }
 }
